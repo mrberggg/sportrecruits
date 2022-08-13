f4d1efd0a1ef67f348b8a115074eaf9c4639955b
Matt Berg <matt.berg95@gmail.com>
2022-08-12 21:32:22 -0700

    Add footer

--- src/components/AcademicFitReport/AcademicFitReport.vue
+++ src/components/AcademicFitReport/AcademicFitReport.vue
@@ -2,6 +2,7 @@
   <v-container v-if="athlete">
     <ReportHeader :athlete="athlete" />
     <ReportTable :athlete="athlete" />
+    <ReportFooter />
   </v-container>
 </template>
 
@@ -9,6 +10,7 @@
 import { defineProps } from "vue";
 import ReportTable from "./ReportTable.vue";
 import ReportHeader from "./ReportHeader.vue";
+import ReportFooter from "./ReportFooter.vue";
 
 defineProps({
   athlete: {
---
+++ src/components/AcademicFitReport/ReportFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-footer :absolute="true" height="35px" class="footer">
+    <v-row class="items">
+      <p>
+        * Rankings for Division I schools based on NCAA data (www.ncaa.com) and
+        rankings for Division II & III schools are based on data from Hero
+        Sports (www.herosports.com/rankings)
+      </p>
+      <p>
+        ** GPA is based on SportsRecruits members who have shown interest in
+        (favorited) the school and have provided their GPA on their profile
+      </p>
+      <p>
+        *** SAT and ACT scores based on national data provided by the National
+        Center of Education Statistics- https://nces.ed.gov/ipeds/
+      </p>
+    </v-row>
+  </v-footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.footer {
+  position: absolute;
+  bottom: 0;
+}
+.items p {
+  font-size: 14px;
+  padding-bottom: 0.75rem;
+}
+</style>
